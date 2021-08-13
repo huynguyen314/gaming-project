35eec07cf39397d0202c18c27668b8e1acc524b2
thieukhanhlinh <novemberlinh@gmail.com>
2021-08-14 00:11:16 +0700

    Update data-generator.py and Update SSIS data ingestion

--- resources/data-generator.py
+++ resources/data-generator.py
@@ -30,7 +30,7 @@ transaction_table_path = f'{dir_path}\\raw-folder\\table-user-transaction-{time_
 
 def create_csv_user_data(record_count):
     # Create user data
-    for i in range(NOW_TIME, NOW_TIME + record_count):
+    for i in range(1000*NOW_TIME, 1000*NOW_TIME + record_count):
         user = fake.user_name()
         email = fake.email()
         age = random.randint(12, 60)
@@ -85,8 +85,8 @@ def create_csv_user_data(record_count):
 
 def create_empty_table():
     with open(user_table_path, 'w', newline='') as userfile:
-        fieldnames = ['UserID','UserName','RegisteredDateID', 'CountryID',
-                      'MembershipID', 'Email', 'Age', 'Gender']
+        fieldnames = ['UserID','UserName','RegisteredDate','RegisteredDateID',
+                      'Membership', 'Email', 'Age', 'Gender']
         writer = csv.DictWriter(userfile, fieldnames=fieldnames)
         writer.writeheader()
 
@@ -101,8 +101,8 @@ def create_empty_table():
         writer.writeheader()
 
     with open(user_transaction_path, 'w', newline='') as transactionfile:
-        fieldnames = ['SessionID', 'UserID', 'CountryID', 'StartDate', 'StartDateID', 'StartTimestamp', 'EndTimestamp',
-                      'CashSpend', 'NoImpression', 'OS', 'OsVersion']
+        fieldnames = ['SessionID', 'UserID', 'Country', 'StartDate', 'StartDateID', 'StartTimestamp', 'EndTimestamp',
+                      'CashSpend', 'NoImpression','eCPM','OS', 'OsVersion']
         writer = csv.DictWriter(transactionfile, fieldnames=fieldnames)
         writer.writeheader()
 
